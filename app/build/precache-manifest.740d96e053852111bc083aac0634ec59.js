7b441255865ab166b9c2b8b6c49fc71475342a71
Salman <apple@Apple-no-MacBook-Pro.local>
2020-04-21 22:34:51 +0600

    moved build folder in to app

---
+++ app/build/precache-manifest.740d96e053852111bc083aac0634ec59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688d124698812e60b1c92f50cc1b4a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633d987f34df015c51be",
+    "url": "/static/css/2.f2b41bb6.chunk.css"
+  },
+  {
+    "revision": "741fe5283dd9fca1a3be",
+    "url": "/static/css/main.a9740d42.chunk.css"
+  },
+  {
+    "revision": "633d987f34df015c51be",
+    "url": "/static/js/2.1ab6cf67.chunk.js"
+  },
+  {
+    "revision": "741fe5283dd9fca1a3be",
+    "url": "/static/js/main.bbd3276f.chunk.js"
+  },
+  {
+    "revision": "10a55dbe21805e4ef18e",
+    "url": "/static/js/runtime-main.acba44bc.js"
+  }
+]);
